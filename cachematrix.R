44c94f6c3f14275a5ff9be7f116e5cad2300dba1
alipsky <andrew.lipsky@gmail.com>
2015-08-21 09:53:09 -0400

    progassign2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  #same format as course example first, init value null
+  z <- NULL
+  set <- function(y) {
+    x <<- y
+    z <<- NULL
+  }
+  #Use get function to select matrix
+  get <- function() x
+  setinverse <-function(inverse) z <<- inverse
+  getinverse <- function() z
+  list (set = set, get = get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getInverse() 
+  if(!is.null(z)) {
+    message("getting cached matrix")
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data)
+  x$setInverse(z)  
+  z                
 }
